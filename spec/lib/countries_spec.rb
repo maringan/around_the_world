282233d4ab529b6b7631d4db115012ebfb3d760b
Kamil Kucharski <kamil.kucharski@connectmedica.com>
2013-03-07 17:20:33 +0100

    more tests :)

--- spec/lib/countries_spec.rb
+++ spec/lib/countries_spec.rb
@@ -1,7 +1,13 @@
 require_relative "../spec_helper"
 
 describe Countries do
-  it "should return correct country full name" do
-    Countries.get_name_by_code("PL\r\n").should == "Poland\r\n"
+  context "Find country name by code" do
+    it "should return correct full country name" do
+      Countries.get_name_by_code("PL\r\n").should == "Poland\r\n"
+    end
+
+    it "should return 'Not found' when key is incorrect" do
+      Countries.get_name_by_code("MagickKey\r\n").should == "Not found\r\n"
+    end
   end
 end
